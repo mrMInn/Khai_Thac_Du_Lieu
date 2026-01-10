0594d2d1b7abe4f7d46a2df7219d1e7c40148f7c
Minh Nguyen <37469990+mrMInn@users.noreply.github.com>
2026-01-10 16:53:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -644,9 +644,9 @@ def render_rough_set():
         st.markdown("##### 6. Liệt kê luật quyết định có độ chính xác 100%")
     if not reducts:
         st.write("Cần tìm Reduct trước khi sinh luật.")
-     else:
-        for i, r in enumerate(reducts):
-         with st.expander(f"Luật từ Reduct {i+1}: {list(r)}", expanded=True):
+    else:
+       for i, r in enumerate(reducts):
+       with st.expander(f"Luật từ Reduct {i+1}: {list(r)}", expanded=True):
                 rules = gen_rules(df, r, decision_attr)
                 
                 if not rules:
